c57db8eedee1e51696a7f5673f2f96944c6f7ebe
jeshi <181858616+Jeshiiiii@users.noreply.github.com>
2025-01-06 17:35:08 +0800

    Sign Library Module
    Jc added his sign library page as well as configuring the package jsons, tyyy

--- salinsign-client/src/App.js
+++ salinsign-client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import LibraryModule from './LibraryModule';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Home Page Route */}
+          <Route
+            path="/"
+            element={
+              <header className="App-header">
+                <p>Welcome to the Home Page!</p>
+                <Link to="/library-module">
+                  <button className="App-button">Go to Sign Library</button>
+                </Link>
+              </header>
+            }
+          />
+          
+          {/* Sign Library Route */}
+          <Route path="/library-module" element={<LibraryModule />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
