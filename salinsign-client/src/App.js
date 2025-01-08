3c887e1942f7356d7ebe3c345adb1efca71a931b
Annric <193468758+Annric25@users.noreply.github.com>
2025-01-08 18:41:18 +0800

    Salin Sign Main Menu
    annric added the main menu including ready routes for translation module, sign library (already routed), Help module. Added logo image (Salin.png) and other images imported to images folder. Modified App.js and App.css.

--- salinsign-client/src/App.js
+++ salinsign-client/src/App.js
@@ -1,31 +1,53 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Link } from 'react-router-dom';
 import './App.css';
 import LibraryModule from './LibraryModule';
 
-function App() {
+const Translate = () => <h2>Translate Module</h2>;
+const SignLibrary = () => <h2>Sign Library Module</h2>;
+const Help = () => <h2>Help Module</h2>;
+
+function MainMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Background-image">
+          <img src="Salin.png" alt="Logo" className="App-logo" />
+          <div className="Button-container">
+            <button onClick={() => navigate('/translate')} className="Menu-button">
+              <img src="TRANSLATE.png" alt="Translate Icon" className="Button-icon" />
+            </button>
+
+            <button onClick={() => navigate('/library-module')} className="Menu-button">
+              <img src="LIBRARY.png" alt="Library Icon" className="Button-icon" />
+            </button>
+
+            <button onClick={() => navigate('/help')} className="Menu-button">
+              <img src="HELP.png" alt="Help Icon" className="Button-icon" />
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+function AppWrapper() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          {/* Home Page Route */}
-          <Route
-            path="/"
-            element={
-              <header className="App-header">
-                <p>Welcome to the Home Page!</p>
-                <Link to="/library-module">
-                  <button className="App-button">Go to Sign Library</button>
-                </Link>
-              </header>
-            }
-          />
-          
-          {/* Sign Library Route */}
-          <Route path="/library-module" element={<LibraryModule />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<MainMenu />} />
+        
+        {/* Routes for other modules */}
+        <Route path="/translate" element={<Translate />} />
+        <Route path="/sign-library" element={<SignLibrary />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/library-module" element={<LibraryModule />} />
+      </Routes>
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
